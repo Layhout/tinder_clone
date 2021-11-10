4c79d4fe3e11776d7ea72aad9ae2abbb48128863
Layhout Chea <layhoutworkmail@gmail.com>
2021-11-10 11:46:57 +0700

    inital push

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import { useNavigation } from '@react-navigation/core'
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+            <Text>Home Screem</Text>
+            <Button title="go to chat screen" onPress={() => navigation.navigate("Chat")} />
+        </View>
+    )
+}
+
+export default HomeScreen
